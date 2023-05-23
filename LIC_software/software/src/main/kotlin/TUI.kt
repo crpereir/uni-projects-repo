9dbd2f149ef2baed3b691269563ccb505d95faa7
carolina <carolinappereir@gmail.com>
2023-05-23 09:51:31 +0100

    files added

---
+++ LIC_software/software/src/main/kotlin/TUI.kt
@@ -0,0 +1,30 @@
+import LCD.clear
+import LCD.cursor
+
+
+object TUI {
+    fun init() {
+        cursor(0, 0)
+    }
+
+    fun setCursor(line: Int, column: Int) {
+        cursor(line, column)
+    }
+
+    fun clearLCD() {
+        clear()
+    }
+
+    fun write() {
+
+    }
+
+    fun read() {
+
+    }
+}
+
+fun main() {
+    TUI.init()
+
+}
