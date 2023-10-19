1e1bc6a69d39b0036f9032da7933fa6101d4f031
carolina <carolinappereir@gmail.com>
2023-10-19 20:45:59 +0100

    comments added

--- TDS/exercicio/src/main/kotlin/BSTree.kt
+++ TDS/exercicio/src/main/kotlin/BSTree.kt
@@ -6,6 +6,7 @@
 interface BSTree<T :Comparable<T>> {
     fun add(e: T): BSTree<T>
     operator fun contains(e: T): Boolean
+
     val height: Int
     fun forEachByOrder(visitor: (T)->Unit )
 }
--- TDS/exercicio/src/main/kotlin/Main.kt
+++ TDS/exercicio/src/main/kotlin/Main.kt
@@ -1,59 +1,69 @@
 
+//Represents a node in a binary search tree.
 data class Node<T : Comparable<T>>(
     val elem: T,
     val left: BSTree<T> = EmptyTree(),
     val right: BSTree<T> = EmptyTree()
 )
 
+fun <T : Comparable<T>> BSTree(): BSTree<T> {
+    return EmptyTree()
+}
+
+// Creates a binary search tree from a list of elements.
+fun <T : Comparable<T>> bsTreeOf(vararg elems: T): BSTree<T> {
+    return elems.fold(BSTree()) { acc, e -> acc.add(e) }
+}
+
+// Defines an empty tree.
 class EmptyTree<T : Comparable<T>> : BSTree<T> {
+
+    // Adds an element to the empty tree.
     override fun add(e: T): BSTree<T> {
-        return NonEmptyTree(Node(e))
+        return NotAnEmptyTree(Node(e))
     }
 
+    // Verifies if an element is contained in the empty tree.
     override operator fun contains(e: T): Boolean {
         return false
     }
 
+    // Defines the height of the empty tree as 0.
     override val height: Int = 0
 
+    // Implements the function that iterates over the elements in the tree in order.
     override fun forEachByOrder(visitor: (T) -> Unit) {
-        TODO("Not yet implemented")
+        return Unit
     }
-
 }
 
-class NonEmptyTree<T : Comparable<T>>(val root: Node<T>) : BSTree<T> {
+// Defines a non-empty tree.
+class NotAnEmptyTree<T : Comparable<T>>(val root: Node<T>) : BSTree<T> {
+    // Adds an element to the non-empty tree.
     override fun add(e: T): BSTree<T> {
         return when {
-            e < root.elem -> NonEmptyTree(root.copy(left = root.left.add(e)))
-            e > root.elem -> NonEmptyTree(root.copy(right = root.right.add(e)))
+            e < root.elem -> NotAnEmptyTree(root.copy(left = root.left.add(e)))
+            e > root.elem -> NotAnEmptyTree(root.copy(right = root.right.add(e)))
             else -> this
         }
     }
 
-    override operator fun contains(e: T): Boolean {
+    // Verifies if an element is contained in the non-empty tree.
+    override operator fun contains(element: T): Boolean {
         return when {
-            e < root.elem -> root.left.contains(e)
-            e > root.elem -> root.right.contains(e)
+            element < root.elem -> root.left.contains(element)
+            element > root.elem -> root.right.contains(element)
             else -> true
         }
     }
 
+    // Calculates the height of the non-empty tree.
     override val height: Int = 1 + maxOf(root.left.height, root.right.height)
 
+    // Iterates over the elements in the tree in order.
     override fun forEachByOrder(visitor: (T) -> Unit) {
         root.left.forEachByOrder(visitor)
         visitor(root.elem)
         root.right.forEachByOrder(visitor)
     }
-}
-
-
-fun <T : Comparable<T>> BSTree(): BSTree<T> {
-    return EmptyTree()
-}
-
-
-fun <T : Comparable<T>> bsTreeOf(vararg elems: T): BSTree<T> {
-    return elems.fold(BSTree()) { acc, e -> acc.add(e) }
 }
